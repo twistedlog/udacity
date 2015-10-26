13a11008045ad1ab67ec5af57e01745a8cfc5fd0
sahil.chug <sahil.chug@fireeye.com>
2015-10-26 19:03:43 +0530

    Completing the first snippet.
    Add mean example
    Add dot product example

--- data_analytics/into-to-data-science/snippets/lesson1/numpy_1_lesson1.py
+++ data_analytics/into-to-data-science/snippets/lesson1/numpy_1_lesson1.py
@@ -53,3 +53,14 @@ if True:
     print array_1 - array_2
     print ""
     print array_1 * array_2
+
+'''
+Numpy mean and dot product
+'''
+if True:
+    array_1 = np.array([1, 2, 3], float)
+    array_2 = np.array([[6], [7], [8]], float)
+    print np.mean(array_1)
+    print np.mean(array_2)
+    print ""
+    print np.dot(array_1, array_2)
