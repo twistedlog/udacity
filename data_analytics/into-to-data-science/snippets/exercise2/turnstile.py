e45772c3ac87b05b7167cb65325bcd5c778cf999
twistedlog <sahil.chug@fireeye.com>
2015-10-31 17:47:37 +0530

    Complete Fixing Turnstile Data

---
+++ data_analytics/into-to-data-science/snippets/exercise2/turnstile.py
@@ -0,0 +1,36 @@
+import numpy as np
+import csv
+
+
+def fix_turnstile_data(filenames):
+    if not(isinstance(filenames, list)):
+        raise TypeError
+
+    for filename in filenames:
+        process(filename)
+
+
+def process(filename):
+    with open(filename, 'r') as f_in:
+        with open('updated_' + filename, 'w') as f_out:
+            reader_in = csv.reader(f_in, delimiter=',')
+            for line in reader_in:
+                _writeline(f_out, _readline(line))
+
+
+def _readline(line):
+    prefix = np.array(line[:3])
+    line = np.array(line[3:])
+    split_factor = len(line) / 5
+    splits = np.split(line, split_factor)
+    for split in splits:
+        return np.concatenate((prefix, split), axis=1).tolist()
+
+
+def _writeline(f_out, line):
+    writer = csv.writer(f_out, delimiter=',')
+    writer.writerow(line)
+
+
+if __name__ == "__main__":
+    fix_turnstile_data(['turnstile_110528.txt'])
